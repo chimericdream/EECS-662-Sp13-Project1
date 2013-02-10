3d639bb1f3aaf44424eba975ca0103ea39eb6c90
Bill Parrott <bill@chimericdream.com>
2013-02-10 12:02:09 -0600

    adding binary operations

--- waee.rkt
+++ waee.rkt
@@ -1,8 +1,10 @@
 #lang plai
+;;;;
+;; WAEE stuff
+;;
 (define-type WAEE
     (num (n number?))
-    (add (lhs WAEE?) (rhs WAEE?))
-    (sub (lhs WAEE?) (rhs WAEE?))
+    (binop (op symbol?) (lhs WAEE?) (rhs WAEE?))
     (with (name symbol?) (named-expr WAEE?) (body WAEE?))
     (id (name symbol?)))
 
@@ -10,8 +12,7 @@
     (lambda (expr sub-id val)
         (type-case WAEE expr
             (num (x) expr)
-            (add (l r) (add (subst l sub-id val) (subst r sub-id val)))
-            (sub (l r) (sub (subst l sub-id val) (subst r sub-id val)))
+            (binop (op l r) (binop op (subst l sub-id val) (subst r sub-id val)))
             (with (bound-id named-expr bound-body)
                 (if (symbol=? bound-id sub-id)
                     (with bound-id (subst named-expr sub-id val) bound-body)
@@ -24,38 +25,59 @@
             ((number? SEXP) (num SEXP))
             ((list? SEXP)
                 (case (first SEXP)
-                    ((+) (add (parse-waee (second SEXP)) (parse-waee (third SEXP))))
-                    ((-) (sub (parse-waee (second SEXP)) (parse-waee (third SEXP))))
-                    ((with) (with (first (second SEXP)) (parse-waee (second (second SEXP))) (parse-waee (third SEXP))))))
+                    ((with) (with (first (second SEXP)) (parse-waee (second (second SEXP))) (parse-waee (third SEXP))))
+                    (else (binop (first SEXP) (parse-waee (second SEXP)) (parse-waee (third SEXP))))))
             ((symbol? SEXP) (id SEXP)))))
 
 (define interp-waee
     (lambda (a-waee)
         (type-case WAEE a-waee
             (num (x) x)
-            (add (l r) (+ (interp-waee l) (interp-waee r)))
-            (sub (l r) (- (interp-waee l) (interp-waee r)))
+            (binop (op l r) ((lookup op ops) (interp-waee l) (interp-waee r)))
             (with (bound-id named-expr bound-body) (interp-waee (subst bound-body bound-id (num (interp-waee named-expr)))))
             (id (v) (error 'interp-waee->id (string-append "Found a free identifier (" (symbol->string v) ")"))))))
 
 (define eval-waee (lambda (SEXP) (interp-waee (parse-waee SEXP))))
 
+;;;;
+;; Binop stuff
+;;
+(define-type Binop
+    (bop (name symbol?) (op procedure?)))
+
+(define lookup
+    (lambda (op-name op-table)
+        (cond ((empty? op-table) (error 'lookup (string-append "Operator not found: " (symbol->string op-name))))
+            (else
+                (if (symbol=? (bop-name (car op-table)) op-name)
+                    (bop-op (car op-table))
+                    (lookup op-name (cdr op-table)))))))
+
+(define get-opname
+    (lambda (op op-table)
+        (cond ((empty? op-table) (error 'lookup (string-append "Operator not found: " (symbol->string op))))
+            (else
+                (if (eq? (bop-op (car op-table)) op)
+                    (bop-name (car op-table))
+                    (lookup op (cdr op-table)))))))
+
+(define ops (list
+    (bop '+ +)
+    (bop '- -)
+    (bop '* *)
+    (bop '/ /)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; returns 5
-(interp-waee (num 5))
-
-; returns 8
-(interp-waee (add (num 3) (num 5)))
-
-; returns 5
-(num-n (num 5))
-
-; returns (num 3)
-(add-lhs (add (num 3) (num 5)))
+;;;;
+;; Tests
+;;
 
 ; returns (with 'x (num 5) (add (id 'x) (id 'x)))
 (parse-waee '{with {x 5} {+ x x}})
+
+; returns -35
+(parse-waee '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
 
 ; returns -35
 (eval-waee '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
@@ -66,5 +88,17 @@
 ; returns 60
 (eval-waee '{with {x 10} {with {y {+ x x}} {with {z {+ y x}} {+ z {+ x y}}}}})
 
+; returns 50
+(eval-waee '{with {x 10} {* x 5}})
+
+; returns 10
+(eval-waee '{/ 50 5})
+
 ; returns an error (free identifier)
-(eval-waee '{with {x 5} {with {x {+ x x}} {+ y x}}})
+;(eval-waee '{with {x 5} {with {x {+ x x}} {+ y x}}})
+
+; test the op-table
+(lookup '+ ops)
+(lookup '- ops)
+(lookup '* ops)
+(lookup '/ ops)
