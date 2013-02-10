f3e0df88c39610ee3fb5aeb667d3f3eaf88233cb
Bill Parrott <bill@chimericdream.com>
2013-02-10 09:53:41 -0600

    renaming structures and functions to waee

--- waee.rkt
+++ waee.rkt
@@ -1,14 +1,14 @@
 #lang plai
-(define-type WAE
+(define-type WAEE
     (num (n number?))
-    (add (lhs WAE?) (rhs WAE?))
-    (sub (lhs WAE?) (rhs WAE?))
-    (with (name symbol?) (named-expr WAE?) (body WAE?))
+    (add (lhs WAEE?) (rhs WAEE?))
+    (sub (lhs WAEE?) (rhs WAEE?))
+    (with (name symbol?) (named-expr WAEE?) (body WAEE?))
     (id (name symbol?)))
 
 (define subst
     (lambda (expr sub-id val)
-        (type-case WAE expr
+        (type-case WAEE expr
             (num (x) expr)
             (add (l r) (add (subst l sub-id val) (subst r sub-id val)))
             (sub (l r) (sub (subst l sub-id val) (subst r sub-id val)))
@@ -18,35 +18,35 @@
                     (with bound-id (subst named-expr sub-id val) (subst bound-body sub-id val))))
             (id (v) (if (symbol=? v sub-id) val expr)))))
 
-(define parse-wae
+(define parse-waee
     (lambda (SEXP)
         (cond
             ((number? SEXP) (num SEXP))
             ((list? SEXP)
                 (case (first SEXP)
-                    ((+) (add (parse-wae (second SEXP)) (parse-wae (third SEXP))))
-                    ((-) (sub (parse-wae (second SEXP)) (parse-wae (third SEXP))))
-                    ((with) (with (first (second SEXP)) (parse-wae (second (second SEXP))) (parse-wae (third SEXP))))))
+                    ((+) (add (parse-waee (second SEXP)) (parse-waee (third SEXP))))
+                    ((-) (sub (parse-waee (second SEXP)) (parse-waee (third SEXP))))
+                    ((with) (with (first (second SEXP)) (parse-waee (second (second SEXP))) (parse-waee (third SEXP))))))
             ((symbol? SEXP) (id SEXP)))))
 
-(define interp-wae
-    (lambda (a-wae)
-        (type-case WAE a-wae
+(define interp-waee
+    (lambda (a-waee)
+        (type-case WAEE a-waee
             (num (x) x)
-            (add (l r) (+ (interp-wae l) (interp-wae r)))
-            (sub (l r) (- (interp-wae l) (interp-wae r)))
-            (with (bound-id named-expr bound-body) (interp-wae (subst bound-body bound-id (num (interp-wae named-expr)))))
-            (id (v) (error 'interp-wae->id (string-append "Found a free identifier (" (symbol->string v) ")"))))))
+            (add (l r) (+ (interp-waee l) (interp-waee r)))
+            (sub (l r) (- (interp-waee l) (interp-waee r)))
+            (with (bound-id named-expr bound-body) (interp-waee (subst bound-body bound-id (num (interp-waee named-expr)))))
+            (id (v) (error 'interp-waee->id (string-append "Found a free identifier (" (symbol->string v) ")"))))))
 
-(define eval-wae (lambda (SEXP) (interp-wae (parse-wae SEXP))))
+(define eval-waee (lambda (SEXP) (interp-waee (parse-waee SEXP))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; returns 5
-(interp-wae (num 5))
+(interp-waee (num 5))
 
 ; returns 8
-(interp-wae (add (num 3) (num 5)))
+(interp-waee (add (num 3) (num 5)))
 
 ; returns 5
 (num-n (num 5))
@@ -55,16 +55,16 @@
 (add-lhs (add (num 3) (num 5)))
 
 ; returns (with 'x (num 5) (add (id 'x) (id 'x)))
-(parse-wae '{with {x 5} {+ x x}})
+(parse-waee '{with {x 5} {+ x x}})
 
 ; returns -35
-(eval-wae '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
+(eval-waee '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
 
 ; returns 20
-(eval-wae '{with {x 5} {with {x {+ x x}} {+ x x}}})
+(eval-waee '{with {x 5} {with {x {+ x x}} {+ x x}}})
 
 ; returns 60
-(eval-wae '{with {x 10} {with {y {+ x x}} {with {z {+ y x}} {+ z {+ x y}}}}})
+(eval-waee '{with {x 10} {with {y {+ x x}} {with {z {+ y x}} {+ z {+ x y}}}}})
 
 ; returns an error (free identifier)
-(eval-wae '{with {x 5} {with {x {+ x x}} {+ y x}}})
+(eval-waee '{with {x 5} {with {x {+ x x}} {+ y x}}})
