8cdbca6fedd7654e051475a73eea4e3bfa8ebd36
Bill Parrott <bill@chimericdream.com>
2013-02-11 00:39:09 -0600

    [waee-multi-with] functional what? (aka... I hate mapping)

--- waee.rkt
+++ waee.rkt
@@ -19,10 +19,45 @@
         (type-case WAEE expr
             (num (x) expr)
             (binop (op l r) (binop op (subst l sub-id val) (subst r sub-id val)))
+
 ;            (with (bound-id named-expr bound-body)
+            (with (bindings bound-body)
+                (with
+                    (map
+                        (lambda (b)
+                            (if (symbol=? (first b) sub-id)
+                                ; bound-id == sub-id
+                                (subst (second b) sub-id val)
+
+                                ; bound-id != sub-id
+                                (
+                                    (subst (second b) sub-id val)
+                                    (subst bound-body sub-id val)
+                                )
+                            )
+                        )
+                        bindings
+                    )
+                    bound-body
+                )
+;                "Do something here"
 ;                (if (symbol=? bound-id sub-id)
-;                    (with bound-id (subst named-expr sub-id val) bound-body)
-;                    (with bound-id (subst named-expr sub-id val) (subst bound-body sub-id val))))
+;                    ; bound-id == sub-id
+;                    (with
+;                        bound-id
+;                        (subst named-expr sub-id val)
+;                        bound-body
+;                    )
+;
+;                    ; bound-id != sub-id
+;                    (with
+;                        bound-id
+;                        (subst named-expr sub-id val)
+;                        (subst bound-body sub-id val)
+;                    )
+;                )
+            )
+;            )
             (id (v) (if (symbol=? v sub-id) val expr)))))
 
 (define parse-waee
@@ -43,7 +78,27 @@
         (type-case WAEE a-waee
             (num (x) x)
             (binop (op l r) ((lookup op ops) (interp-waee l) (interp-waee r)))
-;            (with (bound-id named-expr bound-body) (interp-waee (subst bound-body bound-id (num (interp-waee named-expr)))))
+            (with (bindings bound-body)
+;                "Cycle through the list of bound-ids, and replace each of them in the bound-body with their named expression; then interpret the result"
+                (interp-waee
+                    (
+                        (map
+                            (lambda (b) (subst bound-body (first b) (num (interp-waee (second b)))))
+                            bindings
+                        )
+                        bound-body
+                    )
+                )
+            )
+;            (with (bound-id named-expr bound-body)
+;                (interp-waee
+;                    (subst
+;                        bound-body
+;                        bound-id
+;                        (num (interp-waee named-expr))
+;                    )
+;                )
+;            )
             (id (v) (error 'interp-waee->id (string-append "Found a free identifier (" (symbol->string v) ")"))))))
 
 (define eval-waee (lambda (SEXP) (interp-waee (parse-waee SEXP))))
@@ -77,37 +132,39 @@
 ; returns (with 'x (num 5) (add (id 'x) (id 'x)))
 (parse-waee '{with {{x 5}} {+ x x}})
 
-; returns -35
-(parse-waee '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
+(eval-waee '{with {{x 5}} {+ x x}})
 
 ; returns -35
-(eval-waee '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
+;(parse-waee '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
+
+; returns -35
+;(eval-waee '{+ {- {- 4 3} 15} {+ {+ {- 10 5} {- 3 2}} {- 15 42}}})
 
 ; returns 20
-(eval-waee '{with {{x 5}} {with {{x {+ x x}}} {+ x x}}})
+;(eval-waee '{with {{x 5}} {with {{x {+ x x}}} {+ x x}}})
 
 ; returns 60
-(eval-waee '{with {{x 10}} {with {{y {+ x x}}} {with {{z {+ y x}}} {+ z {+ x y}}}}})
+;(eval-waee '{with {{x 10}} {with {{y {+ x x}}} {with {{z {+ y x}}} {+ z {+ x y}}}}})
 
 ; returns 50
-(eval-waee '{with {{x 10}} {* x 5}})
+;(eval-waee '{with {{x 10}} {* x 5}})
 
 ; returns 10
-(eval-waee '{/ 50 5})
+;(eval-waee '{/ 50 5})
 
 ; returns an error (free identifier)
 ;(eval-waee '{with {x 5} {with {x {+ x x}} {+ y x}}})
 
 ; test the op-table
-(lookup '+ ops)
-(lookup '- ops)
-(lookup '* ops)
-(lookup '/ ops)
+;(lookup '+ ops)
+;(lookup '- ops)
+;(lookup '* ops)
+;(lookup '/ ops)
 
 ; test the binding? check
-(binding? (list 'x (num 5)))
+;(binding? (list 'x (num 5)))
 
 ; test the lob? check
-(lob? (list (list 'x (num 5)) (list 'y (num 7)) (list 'z (num 9))))
+;(lob? (list (list 'x (num 5)) (list 'y (num 7)) (list 'z (num 9))))
 
 (parse-waee '{with {{x 5} {y 7}} {+ x y}})
